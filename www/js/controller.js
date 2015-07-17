d56e7ee6f4750e2b895d9b38e73e7f7dd1a3ffef
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-07-17 10:40:22 -0500

    weird stash state

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,4 +1,6 @@
 angular.module('starter')
-.controller('Soundboard', function() {
+.controller('Soundboard', function($scope) {
+  console.log('yo');
+  $scope.soundbites = [0, 1, 2, 3, 4, 5, 6, 7]
 
 });
