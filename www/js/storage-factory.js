a12b3ae13a5fe16310316a320723693ebdf7821d
calebgregory <calebgregory@gmail.com>
2015-07-17 14:32:57 -0500

    Adding getter and setter functions for all our files from localStorage

--- www/js/storage-factory.js
+++ www/js/storage-factory.js
@@ -1,9 +1,11 @@
 angular.module('storage.factory', [])
 .factory('storage', function(){
   return{
-    var defaultFile = [{
+    defaultFile : [{
       name : 'test',
       file : './sounds/test.mp3'
-    }];
+    }],
+    get files() { return JSON.parse(localStorage.files); },
+    set files(f) { localStorage.files = JSON.stringify(f); }
   }
 })
