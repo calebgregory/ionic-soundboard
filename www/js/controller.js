670e29568cb7ec67e67678a5f87ddff33b46c9a0
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-07-17 15:06:28 -0500

    images

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,8 +1,9 @@
 angular.module('starter')
 .controller('Soundboard', function($scope, sounds, storage) {
   console.log('yo');
-  $scope.soundbites = ['/sounds/Hang-on-baby-Jesus.mp3', '/sounds/Help-me-Tom-Cruise .mp3', '/sounds/I-Piss-Excellence.mp3'];
-
+  $scope.soundbites = ['/sounds/Hang-on-baby-Jesus.mp3', '/sounds/Help-me-Tom-Cruise.mp3', '/sounds/I-Piss-Excellence.mp3'];
+  $scope.rickybobby = ['/img/rickybobby1.jpg', 'img/rickybobby2.jpg', '/img/rickybobby3.jpg'];
+  
   $scope.playSound = function(i){
     $('audio')[i].play();
   }
