dc230ae8dc46e7b48edfaf53db389e2b001aa3c1
calebgregory <calebgregory@gmail.com>
2015-07-17 14:21:23 -0500

    Adding default file to factory

--- www/js/storage-factory.js
+++ www/js/storage-factory.js
@@ -1,6 +1,9 @@
 angular.module('storage.factory', [])
 .factory('storage', function(){
   return{
-
+    var defaultFile = [{
+      name : 'test',
+      file : './sounds/test.mp3'
+    }];
   }
 })
